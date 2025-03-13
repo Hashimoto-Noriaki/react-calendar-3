5a47b9d4aeb43cf66ecf4642be132e1f59a4f8b0
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-03-13 22:08:13 +0900

    ログイン画面作成

--- src/components/pages/LoginPage.tsx
+++ src/components/pages/LoginPage.tsx
@@ -1,23 +1,31 @@
-import React from 'react'
+import { PrimaryBtn } from '../atoms/PrimaryBtn'
+import { NotLoginLayout } from '../templates/NotLoginLayout'
 
 export const LoginPage = () => {
   return (
-    <div>
-        <form className="flex flex-col justify-center items-centers">
-            <h1 className="text-lime-800 text-3xl">ログイン</h1>
-            <div>
-                <input
-                    
-                    placeholder="メールアドレス"
-                />
-            </div>
-            <div>
-                <input
-
-                    placeholder="パスワード"
-                />
-            </div>
-        </form>
-    </div>
+    <NotLoginLayout>
+        <div className="w-[500px] bg-white rounded-lg shadow-lg py-12">
+            <form className="flex flex-col justify-center items-center gap-10">
+                <h1 className="text-lime-800 text-3xl font-bold">ログイン</h1>
+                <div className="w-[80%]">
+                    <input
+                        name="email"
+                        type="text"
+                        className="w-full border-4 border-solid border-lime-800 p-4"
+                        placeholder="メールアドレス"
+                    />
+                </div>
+                <div className="w-[80%]">
+                    <input
+                        name="password"
+                        type="text"
+                        className="w-full border-4 border-solid border-lime-800 p-4"
+                        placeholder="パスワード"
+                    />
+                </div>
+                <PrimaryBtn>ログイン</PrimaryBtn>
+            </form>
+        </div>
+    </NotLoginLayout>
   )
 }
