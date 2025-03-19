05487299feed391208242ebbb8ca6cb64972c667
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2025-03-19 11:46:25 +0900

    カレンダーのテーブル表示(曜日と月)

--- src/components/pages/CalendarPage.tsx
+++ src/components/pages/CalendarPage.tsx
@@ -1,11 +1,22 @@
 import { getMonth } from 'date-fns'
+import { DAYS_LIST } from '../../constants/calendar'
+
 export const CalendarPage = () => {
     const today = new Date()
     return (
         <>
-        <h1 className="font-bold text-3xl mb-5">
-            {`${(getMonth(today))+1}`}
-        </h1>
+        <h1 className="font-bold text-3xl mb-5">{`${(getMonth(today))+1}`}</h1>
+            <table className="w-[80%] border-collapse border-2 border-lime-800 border-solid table-fixed">
+                <thead>
+                    <tr className="bg-lime-800 text-white rounded-tl-lg rounded-tr-lg py-10">
+                        {DAYS_LIST.map((today) => (
+                            <th key={today} className="text-center text-xl py-3">
+                                {today}
+                            </th>
+                        ))}
+                    </tr>
+                </thead>
+            </table>
         </>
     )
 }
